41b33e5412fb8910302774cff6d6ebacd0d51c54
Anita Siemayr <siemayr.anita@gmx.at>
2015-05-07 18:03:20 +0200

    twitter is now working with test message

--- src/main/java/cinema/service/CoffeeService.java
+++ src/main/java/cinema/service/CoffeeService.java
@@ -1,9 +1,5 @@
 package cinema.service;
 
-/**
- * Created by Daniel on 06.05.2015.
- */
-
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -21,7 +17,10 @@ public class CoffeeService {
 
         camelContext.addRoutes(new RouteBuilder() {
             public void configure() {
-                from("file:tmp/in?noop=true").to("file:tmp/out");
+                from("file:tmp/in?noop=true")
+                        .to("file:tmp/out")
+                        .to("twitter://timeline/user?consumerKey=4ztAqgtOpu61vyuUiLxqoI2xV&consumerSecret=9uuh7Vm7KtTmlyt2QDADLzYOaX0VkkRSfbtLwf8pPxDMS6THbB&accessToken=3240346329-PdlJTNwUHre4YW5ySic7x1505NaCZCTfC4JCheM&accessTokenSecret=FnLGjfjm9raE5Pyrs35XK5C5xb3recJ6Rg5TtZyLYEI4f");
+
             }});
 
         camelContext.start();
