8767874486becd593056aaef938b05e8707ba328
scheickl <s.scheickl@gmx.net>
2015-05-13 10:11:19 +0200

    facebook

--- src/main/java/cinema/service/SocialMediaService.java
+++ src/main/java/cinema/service/SocialMediaService.java
@@ -31,7 +31,7 @@ public class SocialMediaService {
                 conf.setOAuthAccessToken("CAAK8TT51AOQBAOLIiOOCMdB33SXZCOU55S6iGn2juLgtaXlOurCcdgDvFCga3GZCzKhZBECpRZC7kjrPACVJYBdw04SbLufsujZCbGoemWjKUFwjFzEHt1A0FQyaSXPjsyWsEFp8bVr48PByOMOGqSAZBfloTNGNe407PKOd0OePjJp2m0hVGb8Bj8NVZCOUVNFt4OSJVKmjLvZAdqnTr3vm");
                 conf.setOAuthAppId("769989899780324");
                 conf.setOAuthAppSecret("1b3f6821328040af2e017b685b72ea5d");
-                Endpoint end = new FacebookEndpoint("facebook://postFeed?message=abc", new FacebookComponent(camelContext, conf), "", new FacebookEndpointConfiguration());
+                Endpoint end = new FacebookEndpoint("facebook://postFeed?inBody=abc", new FacebookComponent(camelContext, conf), "", new FacebookEndpointConfiguration());
 
                 from("file:tmp/in").to(end);
                 //from("file:tmp/in").to("facebook://postFeed?message=abc&oAuthAppId=1643747599195694&oAuthAppSecret=b4308c6d220393a71e3b9d6ccfb4bcd3&oAuthAccessToken=CAAK8TT51AOQBAGwqhWBrt6Ss21Y4JEiXHsjVzs18j0U4mZB4cofqi1GbHJsOZBCaUYf5GMfbaBvD19Yf7O0Im5OI4EBZCrRygy6sxJVVQpNtXMt5IN75WEpnHkoGa9ZAxdlt2xyY2kPiWmN4I0FYHXjw7dl5o6PnTlMj4prvnR72ZCX47W9OmC0SGRpiagWIHsnKbJWFu9vfwIa61SZCLZB");
@@ -40,7 +40,7 @@ public class SocialMediaService {
         });
         ProducerTemplate template = camelContext.createProducerTemplate();
         camelContext.start();
-        template.sendBody("facebook://postFeed?message=abc&oAuthAppId=769989899780324&oAuthAppSecret=1b3f6821328040af2e017b685b72ea5d&oAuthAccessToken=CAAK8TT51AOQBADQrwNNIUNc39ofPMCJsdbwZA71ZAiod3d9uH8gZBuGUMP8Xm2BXOFeC0mBXFu3fT3ZCZBTiOLbCiAQjCVDwKtODMJuDE6DJaPbAhHHTuNsbCIo92NZBtsddQ0kZAHsva8Ru8HddUGu4yX2IIVnZBVCVdfl1lYRr1MkIOIcn6CdFL3mjZAYVukAMaSW9fmYp6ttZCnfrVdZBJha", "Test Message: ");
+        template.sendBody("facebook://postFeed?inBody=abc&oAuthAppId=769989899780324&oAuthAppSecret=1b3f6821328040af2e017b685b72ea5d&oAuthAccessToken=CAAK8TT51AOQBADQrwNNIUNc39ofPMCJsdbwZA71ZAiod3d9uH8gZBuGUMP8Xm2BXOFeC0mBXFu3fT3ZCZBTiOLbCiAQjCVDwKtODMJuDE6DJaPbAhHHTuNsbCIo92NZBtsddQ0kZAHsva8Ru8HddUGu4yX2IIVnZBVCVdfl1lYRr1MkIOIcn6CdFL3mjZAYVukAMaSW9fmYp6ttZCnfrVdZBJha", "Test Message: ");
         Thread.sleep(60 * 5 * 1000);
         camelContext.stop();
 
