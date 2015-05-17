78a129d9827a3af71b198dda6375df6846747e2a
Daniel <Daniel.Shatkin@gmail.com>
2015-05-17 21:49:37 +0200

    changed mongoroute

--- src/main/java/cinema/routes/CamelMongoRoute.java
+++ src/main/java/cinema/routes/CamelMongoRoute.java
@@ -1,18 +1,29 @@
 package cinema.routes;
 
+import com.mongodb.Mongo;
+import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.SimpleRegistry;
+import org.apache.camel.model.dataformat.XmlJsonDataFormat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Daniel on 11.05.2015.
  */
+@Component
 public class CamelMongoRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("http:http://localhost:8080/mongo")
-                       .to("mongodb:mongoBean?database=workflow&collection=workflow&operation=insert");
 
-        System.out.println("#####wtf####");
+        final XmlJsonDataFormat xmlJsonFormat = new XmlJsonDataFormat();
+        xmlJsonFormat.setForceTopLevelObject(true);
 
+        from("file:tmp/mongo?noop=true").marshal(xmlJsonFormat).convertBodyTo(String.class)
+                .to("mongodb:mongoBean?database=workflow&collection=workflow&operation=insert")
+                .to("file:tmp/mongoout");
     }
+
 }
