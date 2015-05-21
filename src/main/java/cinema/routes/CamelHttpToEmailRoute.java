b7b86e69345d7e2636877ad7eb6e0a46ee47da96
Daniel <Daniel.Shatkin@gmail.com>
2015-05-21 18:22:41 +0200

    Email confirmation done!

--- src/main/java/cinema/routes/CamelHttpToEmailRoute.java
+++ src/main/java/cinema/routes/CamelHttpToEmailRoute.java
@@ -1,7 +1,9 @@
 package cinema.routes;
 
 import cinema.dto.TicketDTO;
+import cinema.model.Ticket;
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.model.dataformat.XmlJsonDataFormat;
@@ -16,9 +18,23 @@ public class CamelHttpToEmailRoute extends RouteBuilder {
 
         from("direct:mail")
                 .unmarshal().json(JsonLibrary.Jackson, TicketDTO.class)
-                .log("${body}")
-                .log("######willwritemail")
-                .recipientList(simple("smtps://smtp.gmail.com?username=moviecenter.wmpm@gmail.com&password=workflow&to=daniel.shatkin@gmail.com&subject=Your reservation"));
+                .process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        TicketDTO ticketDTO = (TicketDTO) exchange.getIn().getBody();
+                        Ticket ticket = ticketDTO.getTicket();
+
+                        exchange.getProperties().put("mail",ticket.getMail());
+
+                        String body = "Thank you for your reservation at the movie Theater \n"   +
+                                "for the movie" + ticket.getScreening().getMovie().getMovieName()+ "!\n"+
+                                "Your Customer Number is: " + ticket.getCustomerId() +      "\n" +
+                                "The price for all of your reserved tickets is: " + ticket.getOverallPrice().toString() + ".";
+                        exchange.getIn().setBody(body);
+                    }
+                })
+                .log("${property[mail]}")
+                .recipientList(simple("smtps://smtp.gmail.com?username=moviecenter.wmpm@gmail.com&password=workflow&to=${property[mail]}&subject=Your reservation."));
 
     }
 }
