a59f5407951b0152fcf4e65a6e7a5be2261fa33e
Daniel <Daniel.Shatkin@gmail.com>
2015-05-21 21:05:50 +0200

    deleted commented code

--- src/main/java/cinema/routes/CamelMongoRoute.java
+++ src/main/java/cinema/routes/CamelMongoRoute.java
@@ -32,25 +32,7 @@ public class CamelMongoRoute extends RouteBuilder {
                 .convertBodyTo(String.class)
                 .log("to String")
                 .to("mongodb:mongoBean?database=workflow&collection=workflow&operation=insert").log("written to mongoDB");
-                /*.unmarshal(format)
-        //from("file://src/main/resources/test?noop=true")
-                        .recipientList(simple("smtps://smtp.gmail.com?username=moviecenter.wmpm@gmail.com&password=workflow&to=s.scheickl@gmx.net&subject=Your reservation"));
-        ;*/
+
     }
-
-/*
-
-    @Override
-    public void configure() throws Exception {
-
-        final XmlJsonDataFormat xmlJsonFormat = new XmlJsonDataFormat();
-        xmlJsonFormat.setForceTopLevelObject(true);
-        from("timer://foo?fixedRate=true&delay=0&period=10000")
-                .to("http4://localhost/mon")
-                        .to("mongodb:mongoBean?database=workflow&collection=workflow&operation=insert")
-                .to("file:tmp/mongoout");
-    }
-*/
-
 
 }
