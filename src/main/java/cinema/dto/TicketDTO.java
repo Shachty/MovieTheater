22567e8ff1f41e1a5d5b3312969b98488de6d58c
Daniel <Daniel.Shatkin@gmail.com>
2015-05-25 19:28:37 +0200

    dtos

--- src/main/java/cinema/dto/TicketDTO.java
+++ src/main/java/cinema/dto/TicketDTO.java
@@ -3,6 +3,7 @@ package cinema.dto;
 import cinema.model.Ticket;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.bson.types.ObjectId;
 
 /**
  * Created by Daniel on 21.05.2015.
---
+++ src/main/java/cinema/model/TicketStatus.java
@@ -0,0 +1,9 @@
+package cinema.model;
+
+/**
+ * Created by Daniel on 24.05.2015.
+ */
+public enum TicketStatus {
+
+    RESERVATION_IN_PROGRESS,BUYING_IN_PROGRESS,INVALID,FULL,GOOD
+}
